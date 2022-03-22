c9f9b10ba3f56101d02cf4794745648a7552eb2e
Philippe Collet <Philippe.Collet@unice.fr>
2022-03-22 09:27:55 +0100

    Separation of all DB properties + activation of JPA/SQL tracing

--- backend/src/main/java/fr/univcotedazur/simpletcfs/configurations/PersistenceJpaConfig.java
+++ backend/src/main/java/fr/univcotedazur/simpletcfs/configurations/PersistenceJpaConfig.java
@@ -1,6 +1,5 @@
 package fr.univcotedazur.simpletcfs.configurations;
 
-import fr.univcotedazur.simpletcfs.components.Cart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
